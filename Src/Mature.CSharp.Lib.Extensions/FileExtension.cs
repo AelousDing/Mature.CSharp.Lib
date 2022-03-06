c56b9c09ef93f614be86f77041c2671692ec5af0
fengjuanchensha <1130217572@qq.com>
2022-03-06 19:31:39 +0800

    添加扩展
    添加扩展

---
+++ Src/Mature.CSharp.Lib.Extensions/FileExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Mature.CSharp.Lib.Extensions
+{
+    public static class FileExtension
+    {
+        public static string GetDirectoryName(this string exePath)
+        {
+            int index = exePath.LastIndexOf('\\');
+            return exePath.Substring(index + 1);
+
+        }
+        public static string GetFileName(this string exePath)
+        {
+            var name = exePath.Split('\\').LastOrDefault();
+            if (string.IsNullOrEmpty(name))
+            {
+                return "";
+            }
+            int index = name.LastIndexOf(".");
+            return name.Substring(0, index);
+        }
+    }
+}
---
+++ Src/Mature.CSharp.Lib.Extensions/ProcessExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Mature.CSharp.Lib.Extensions
+{
+    public class ProcessExtension
+    {
+        public static bool Start(string fileName, string args, int milliseconds, out string output)
+        {
+            Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = fileName;
+            p.StartInfo.Arguments = args;
+
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = false;
+            p.Start();
+            output = p.StandardOutput.ReadToEnd();
+            return p.WaitForExit(milliseconds);
+        }
+    }
+}
